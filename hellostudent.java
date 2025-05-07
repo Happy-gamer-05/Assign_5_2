ab7c5cb58b96b5c5f6ebf1a024d9b8d4153220fe
happy-gamer-05 <hg0526104@gmail.com>
2025-05-08 03:09:55 +0530

    3rd commit

--- hellostudent.java
+++ hellostudent.java
@@ -2,5 +2,6 @@ public class hellostudent {
 	public static void main(String[] args) {
 		System.out.println("Hello, Utkarsh !!");
 		System.out.println("Welcome to C-DAC");
+		System.out.println("Welcome to Ditiss");
 	}
 }
