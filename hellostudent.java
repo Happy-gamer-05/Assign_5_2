8e590ee2d959e068f3b78dbd291ac08123589139
happy-gamer-05 <hg0526104@gmail.com>
2025-05-08 02:21:25 +0530

    2nd commit

--- hellostudent.java
+++ hellostudent.java
@@ -1,4 +1,4 @@
-public class helloworld {
+public class hellostudent {
 	public static void main(String[] args) {
 		System.out.println("Hello, Utkarsh !!");
 		System.out.println("Welcome to C-DAC");
